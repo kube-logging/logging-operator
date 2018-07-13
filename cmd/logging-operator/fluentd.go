5663d8f38fdd4161cbd8172ab742dcfb626f6841
Balint Molnar <balintmolnar91@gmail.com>
2018-07-13 15:36:06 +0200

    GoFmt files

--- cmd/logging-operator/fluentd.go
+++ cmd/logging-operator/fluentd.go
@@ -1,43 +1,43 @@
 package main
 
 import (
-    corev1 "k8s.io/api/core/v1"
-    extensionv1 "k8s.io/api/extensions/v1beta1"
-    "k8s.io/apimachinery/pkg/api/resource"
-    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-    "k8s.io/apimachinery/pkg/util/intstr"
+	corev1 "k8s.io/api/core/v1"
+	extensionv1 "k8s.io/api/extensions/v1beta1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
 func newFluentdRole() {
 
 }
 func newFluentdService() *corev1.Service {
-    return &corev1.Service{
-        TypeMeta: metav1.TypeMeta{
-            Kind:       "Service",
-            APIVersion: "v1",
-        },
-        ObjectMeta: metav1.ObjectMeta{
-            Name:      "fluentd",
-            Namespace: "default",
-            Labels:    map[string]string {
-                "app": "fluentd",
-            },
-        },
-        Spec: corev1.ServiceSpec{
-            Ports:                    []corev1.ServicePort{
-                {
-                    Protocol:   corev1.ProtocolTCP,
-                    Port:       24240,
-                    TargetPort: intstr.IntOrString{IntVal: 24240},
-                },
-            },
-            Selector:    map[string]string{
-                "app": "fluentd",
-            },
-            Type:        "ClusterIP",
-        },
-    }
+	return &corev1.Service{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Service",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "fluentd",
+			Namespace: "default",
+			Labels: map[string]string{
+				"app": "fluentd",
+			},
+		},
+		Spec: corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{
+				{
+					Protocol:   corev1.ProtocolTCP,
+					Port:       24240,
+					TargetPort: intstr.IntOrString{IntVal: 24240},
+				},
+			},
+			Selector: map[string]string{
+				"app": "fluentd",
+			},
+			Type: "ClusterIP",
+		},
+	}
 
 }
 
@@ -45,8 +45,8 @@ func newFluentdService() *corev1.Service {
 
 // TODO This has to be a Golang template with proper values gathered
 func newFluentdConfigmap() *corev1.ConfigMap {
-    config :=
-`# Prevent fluentd from handling records containing its own logs. Otherwise
+	config :=
+		`# Prevent fluentd from handling records containing its own logs. Otherwise
 # it can lead to an infinite loop, when error in sending one message generates
 # another message which also fails to be sent and so on.
 <match fluentd.**>
@@ -92,137 +92,137 @@ func newFluentdConfigmap() *corev1.ConfigMap {
     @type null
 </match>
 `
-    configMap := &corev1.ConfigMap{
-        TypeMeta: metav1.TypeMeta{
-            Kind:       "ConfigMap",
-            APIVersion: "v1",
-        },
-        ObjectMeta: metav1.ObjectMeta{
-            Name:      "fluentd-config",
-            Namespace: "default",
-            Labels:    labels,
-        },
+	configMap := &corev1.ConfigMap{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ConfigMap",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "fluentd-config",
+			Namespace: "default",
+			Labels:    labels,
+		},
 
-        Data: map[string]string{
-            "example.conf": config,
-        },
-    }
-    return configMap
+		Data: map[string]string{
+			"example.conf": config,
+		},
+	}
+	return configMap
 }
 
 type fluentdDeploymentConfig struct {
-    Namespace string
-    Replicas  int32
+	Namespace string
+	Replicas  int32
 }
 
 func newFluentdPVC() *corev1.PersistentVolumeClaim {
-    return &corev1.PersistentVolumeClaim{
-        TypeMeta: metav1.TypeMeta{
-            Kind:       "PersistentVolumeClaim",
-            APIVersion: "v1",
-        },
-        ObjectMeta: metav1.ObjectMeta{
-            Name:      "fluentd-buffer",
-            Namespace: "default",
-            Labels:    labels,
-        },
-        Spec: corev1.PersistentVolumeClaimSpec{
-            AccessModes: []corev1.PersistentVolumeAccessMode{
-                "ReadWriteOnce",
-            },
-            Resources: corev1.ResourceRequirements{
-                Requests: corev1.ResourceList{
-                    "storage": resource.MustParse("10G"),
-                },
-            },
-        },
-    }
+	return &corev1.PersistentVolumeClaim{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "PersistentVolumeClaim",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "fluentd-buffer",
+			Namespace: "default",
+			Labels:    labels,
+		},
+		Spec: corev1.PersistentVolumeClaimSpec{
+			AccessModes: []corev1.PersistentVolumeAccessMode{
+				"ReadWriteOnce",
+			},
+			Resources: corev1.ResourceRequirements{
+				Requests: corev1.ResourceList{
+					"storage": resource.MustParse("10G"),
+				},
+			},
+		},
+	}
 }
 
 // /fluentd/etc/fluent.conf  is the default config
 // TODO the options should come from the operator configuration
 func newFluentdDeployment() *extensionv1.Deployment {
-    labels := map[string]string{
-        "app": "fluentd",
-    }
-    var replicas int32 = 1
-    return &extensionv1.Deployment{
-        TypeMeta: metav1.TypeMeta{
-            Kind:       "Deployment",
-            APIVersion: "extensions/v1beta1",
-        },
-        ObjectMeta: metav1.ObjectMeta{
-            Name:      "fluentd",
-            Namespace: "default",
-            Labels:    labels,
-        },
-        Spec: extensionv1.DeploymentSpec{
-            Replicas: &replicas,
-            Template: corev1.PodTemplateSpec{
-                ObjectMeta: metav1.ObjectMeta{
-                    Name:   "fluent-bit",
-                    Labels: labels,
-                    // TODO Move annotations to configuration
-                    Annotations: map[string]string{
-                        "prometheus.io/scrape": "true",
-                        "prometheus.io/path":   "/metrics",
-                        "prometheus.io/port":   "25000",
-                    },
-                },
-                Spec: corev1.PodSpec{
-                    Volumes: []corev1.Volume{
-                        {
-                            Name: "config",
-                            VolumeSource: corev1.VolumeSource{
-                                ConfigMap: &corev1.ConfigMapVolumeSource{
-                                    LocalObjectReference: corev1.LocalObjectReference{
-                                        Name: "fluentd-config",
-                                    },
-                                },
-                            },
-                        },
-                        {
-                            Name: "buffer",
-                            VolumeSource: corev1.VolumeSource{
-                                PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
-                                    ClaimName: "fluentd-buffer",
-                                    ReadOnly:  false,
-                                },
-                            },
-                        },
-                    },
-                    Containers: []corev1.Container{
-                        {
-                            Name:  "fluentd",
-                            Image: "banzaicloud/fluentd:v1.1.2",
-                            Ports: []corev1.ContainerPort{
-                                {
-                                    Name:          "monitor",
-                                    ContainerPort: 25000,
-                                    Protocol:      "TCP",
-                                },
-                                {
-                                    Name:          "fluent-input",
-                                    ContainerPort: 24224,
-                                    Protocol:      "TCP",
-                                },
-                            },
+	labels := map[string]string{
+		"app": "fluentd",
+	}
+	var replicas int32 = 1
+	return &extensionv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Deployment",
+			APIVersion: "extensions/v1beta1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "fluentd",
+			Namespace: "default",
+			Labels:    labels,
+		},
+		Spec: extensionv1.DeploymentSpec{
+			Replicas: &replicas,
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:   "fluent-bit",
+					Labels: labels,
+					// TODO Move annotations to configuration
+					Annotations: map[string]string{
+						"prometheus.io/scrape": "true",
+						"prometheus.io/path":   "/metrics",
+						"prometheus.io/port":   "25000",
+					},
+				},
+				Spec: corev1.PodSpec{
+					Volumes: []corev1.Volume{
+						{
+							Name: "config",
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{
+										Name: "fluentd-config",
+									},
+								},
+							},
+						},
+						{
+							Name: "buffer",
+							VolumeSource: corev1.VolumeSource{
+								PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+									ClaimName: "fluentd-buffer",
+									ReadOnly:  false,
+								},
+							},
+						},
+					},
+					Containers: []corev1.Container{
+						{
+							Name:  "fluentd",
+							Image: "banzaicloud/fluentd:v1.1.2",
+							Ports: []corev1.ContainerPort{
+								{
+									Name:          "monitor",
+									ContainerPort: 25000,
+									Protocol:      "TCP",
+								},
+								{
+									Name:          "fluent-input",
+									ContainerPort: 24224,
+									Protocol:      "TCP",
+								},
+							},
 
-                            VolumeMounts: []corev1.VolumeMount{
-                                {
-                                    Name:      "config",
-                                    MountPath: "/fluentd/etc/conf.d",
-                                },
-                                {
-                                    Name:      "buffer",
-                                    MountPath: "/buffers",
-                                },
-                            },
-                        },
-                    },
-                    //ServiceAccountName: "",
-                },
-            },
-        },
-    }
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      "config",
+									MountPath: "/fluentd/etc/conf.d",
+								},
+								{
+									Name:      "buffer",
+									MountPath: "/buffers",
+								},
+							},
+						},
+					},
+					//ServiceAccountName: "",
+				},
+			},
+		},
+	}
 }
--- cmd/logging-operator/main.go
+++ cmd/logging-operator/main.go
@@ -1,78 +1,78 @@
 package main
 
 import (
-    "context"
-    "runtime"
+	"context"
+	"runtime"
 
-    "github.com/banzaicloud/logging-operator/pkg/stub"
-    "github.com/operator-framework/operator-sdk/pkg/sdk"
-    sdkVersion "github.com/operator-framework/operator-sdk/version"
+	"github.com/banzaicloud/logging-operator/pkg/stub"
+	"github.com/operator-framework/operator-sdk/pkg/sdk"
+	sdkVersion "github.com/operator-framework/operator-sdk/version"
 
-    "github.com/sirupsen/logrus"
-    "os"
+	"github.com/sirupsen/logrus"
+	"os"
 )
 
 func printVersion(namespace string) {
-    logrus.Infof("Go Version: %s", runtime.Version())
-    logrus.Infof("Go OS/Arch: %s/%s", runtime.GOOS, runtime.GOARCH)
-    logrus.Infof("operator-sdk Version: %v", sdkVersion.Version)
-    logrus.Infof("Operator namespace: %s", namespace)
+	logrus.Infof("Go Version: %s", runtime.Version())
+	logrus.Infof("Go OS/Arch: %s/%s", runtime.GOOS, runtime.GOARCH)
+	logrus.Infof("operator-sdk Version: %v", sdkVersion.Version)
+	logrus.Infof("Operator namespace: %s", namespace)
 }
 
 func getConfiguration() *operatorConfig {
-    return &operatorConfig{}
+	return &operatorConfig{}
 }
 
 func main() {
 
-    const (
-        operatorNamespace = "OPERATOR_NAMESPACE"
-        resource = "logging.banzaicloud.com/v1alpha1"
-        kind = "LoggingOperator"
-    )
+	const (
+		operatorNamespace = "OPERATOR_NAMESPACE"
+		resource          = "logging.banzaicloud.com/v1alpha1"
+		kind              = "LoggingOperator"
+	)
 
-    ns := os.Getenv(operatorNamespace)
-    printVersion(ns)
+	ns := os.Getenv(operatorNamespace)
+	printVersion(ns)
 
-    logrus.Info("Deploy fluent-bit")
-    initFluentBit()
-    logrus.Info("Deploy fluentd")
-    initFluentd()
-    resyncPeriod := 5
-    logrus.Infof("Watching %s, %s, %s, %d", resource, kind, ns, resyncPeriod)
-    sdk.Watch(resource, kind, ns, resyncPeriod)
-    sdk.Handle(stub.NewHandler())
-    sdk.Run(context.TODO())
+	logrus.Info("Deploy fluent-bit")
+	initFluentBit()
+	logrus.Info("Deploy fluentd")
+	initFluentd()
+	resyncPeriod := 5
+	logrus.Infof("Watching %s, %s, %s, %d", resource, kind, ns, resyncPeriod)
+	sdk.Watch(resource, kind, ns, resyncPeriod)
+	sdk.Handle(stub.NewHandler())
+	sdk.Run(context.TODO())
 }
 
 type operatorConfig struct {
 }
 
 func initFluentBit() {
-    cfg := &fluentBitDeploymentConfig{
-        Namespace: "default",
-    }
-    sdk.Create(newServiceAccount(cfg))
-    sdk.Create(newClusterRole(cfg))
-    sdk.Create(newClusterRoleBinding(cfg))
-    cfgMap, _ := newFluentBitConfig(cfg)
-    sdk.Create(cfgMap)
-    sdk.Create(newFluentBitDaemonSet(cfg))
+	cfg := &fluentBitDeploymentConfig{
+		Namespace: "default",
+	}
+	sdk.Create(newServiceAccount(cfg))
+	sdk.Create(newClusterRole(cfg))
+	sdk.Create(newClusterRoleBinding(cfg))
+	cfgMap, _ := newFluentBitConfig(cfg)
+	sdk.Create(cfgMap)
+	sdk.Create(newFluentBitDaemonSet(cfg))
 }
 
 func initFluentd() {
-    sdk.Create(newFluentdConfigmap())
-    sdk.Create(newFluentdPVC())
-    sdk.Create(newFluentdDeployment())
-    sdk.Create(newFluentdService())
-    // Create fluentd services
-    // Possible options
-    //  replica: x
-    //  tag_rewrite config: ? it should be possible to give labels
-    //  input port
-    //  TLS?
-    //  monitoring
-    //    enabled:
-    //    port:
-    //    path:
+	sdk.Create(newFluentdConfigmap())
+	sdk.Create(newFluentdPVC())
+	sdk.Create(newFluentdDeployment())
+	sdk.Create(newFluentdService())
+	// Create fluentd services
+	// Possible options
+	//  replica: x
+	//  tag_rewrite config: ? it should be possible to give labels
+	//  input port
+	//  TLS?
+	//  monitoring
+	//    enabled:
+	//    port:
+	//    path:
 }
