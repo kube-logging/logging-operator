8000f64ac673b8819e9ec4bedfd363b60b76f939
tarokkk <gubasanyi@gmail.com>
2018-07-24 11:55:55 +0200

    crd alpha version

---
+++ pkg/apis/logging/v1alpha1/types_test.go
@@ -0,0 +1,56 @@
+package v1alpha1
+
+import (
+	"testing"
+	"k8s.io/apimachinery/pkg/util/yaml"
+	"encoding/json"
+	"fmt"
+)
+
+var rawData = []byte(`
+apiVersion: "logging.banzaicloud.com/v1alpha1"
+kind: "LoggingOperator"
+metadata:
+  name: "nginx-logging"
+spec:
+  input:
+    label:
+      app-label: nginx
+  filter:
+    - type: parse
+      format: '/^(?<remote>[^ ]*) (?<host>[^ ]*) (?<user>[^ ]*) \[(?<time>[^\]]*)\] "(?<method>\S+)(?: +(?<path>[^\"]*) +\S*)?" (?<code>[^ ]*) (?<size>[^ ]*)(?: "(?<referer>[^\"]*)" "(?<agent>[^\"]*)")?$/'
+      timeFormat: "%d/%b/%Y:%H:%M:%S %z"
+  output:
+    - s3:
+        name: adasd
+        params:
+          - name: aws_key_id
+            valueFrom:
+              secretKeyRef:
+                name: loggingS3
+                key: AWS_ACCESS_KEY_ID
+          - name: aws_sec_key
+            valueFrom:
+              secretKeyRef:
+                name: loggingS3
+                key: AWS_SECRET_ACCESS_KEY
+          - name: s3_bucket
+            value: logging-bucket
+          - name: s3_region
+            value: ap-northeast-1
+`)
+
+
+func TestExampleCRD(t *testing.T) {
+	output := LoggingOperator{}
+	jsonData, err := yaml.ToJSON(rawData)
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(jsonData)
+	err = json.Unmarshal(jsonData, &output)
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Printf("%#v", output)
+}
--- pkg/stub/handler.go
+++ pkg/stub/handler.go
@@ -3,9 +3,12 @@ package stub
 import (
 	"context"
 
+	"fmt"
 	"github.com/banzaicloud/logging-operator/pkg/apis/logging/v1alpha1"
 	"github.com/operator-framework/operator-sdk/pkg/sdk"
 	"github.com/sirupsen/logrus"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // NewHandler creates a new Handler struct
@@ -22,53 +25,73 @@ type Handler struct {
 func (h *Handler) Handle(ctx context.Context, event sdk.Event) (err error) {
 	switch o := event.Object.(type) {
 	case *v1alpha1.LoggingOperator:
-		logrus.Infof("New CRD arrived %#v", o)
+		logrus.Info("New CRD arrived %#v", o)
+		logrus.Info("Generating configuration.")
+		name, config := generateFluentdConfig(o)
+		if config != "" && name != "" {
+			updateConfigMap(name, config)
+		}
 	}
 	return
 }
 
+func updateConfigMap(name, config string) {
+	configMap := &corev1.ConfigMap{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ConfigMap",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "app-config",
+			Namespace: "default",
+		},
+	}
+	err := sdk.Get(configMap)
+	if err != nil {
+		logrus.Error(err)
+	}
+	if configMap.Data == nil {
+		configMap.Data = map[string]string{}
+	}
+	configMap.Data[name] = config
+	err = sdk.Update(configMap)
+	if err != nil {
+		logrus.Error(err)
+	}
+}
+
 //
-//generateFluentdConfig(*v1alpha1.LoggingOperator) {
-//
-//}
+func generateFluentdConfig(crd *v1alpha1.LoggingOperator) (string, string) {
+	var finalConfig string
+	// Create pattern for match
+	baseMap := map[string]string{}
+	baseMap["pattern"] = crd.Spec.Input.Label["app-label"]
+	// Generate filters
+	for _, filter := range crd.Spec.Filter {
+		logrus.Info("Applying filter")
+		values := filter.GetMap()
+		values["pattern"] = crd.Spec.Input.Label["app-label"]
+		config, err := v1alpha1.RenderParser(values)
+		if err != nil {
+			logrus.Info("Error in rendering template.")
+			return "", ""
+		}
+		fmt.Println(config)
+		finalConfig += config
+	}
 
-type loggingOperatorCRD struct {
-	Input  inputFluentd
-	Filter filterFluentd
-}
+	// Generate output
+	for _, output := range crd.Spec.Output {
+		values := output.S3.GetMap()
+		values["pattern"] = crd.Spec.Input.Label["app-label"]
+		config, err := v1alpha1.RenderS3(values)
+		if err != nil {
+			logrus.Info("Error in rendering template.")
+			return "", ""
+		}
+		fmt.Println(config)
+		finalConfig += config
+	}
+	return baseMap["pattern"], finalConfig
 
-type inputFluentd struct {
-	Label string
-	Value string
-}
-
-type filterFluentd struct {
-	Name       string
-	Format     string
-	TimeFormat string
-}
-
-type outputFluentd struct {
-	S3 outputS3
-}
-
-type outputS3 struct {
-	Parameters Parameters
-}
-
-// Parameters struct
-type Parameters struct {
-	Name      string
-	ValueFrom ValueFrom
-	Value     string
-}
-
-// ValueFrom struct
-type ValueFrom struct {
-	SecretKeyRef kubernetesSecret
-}
-
-type kubernetesSecret struct {
-	Name string
-	Key  string
 }
