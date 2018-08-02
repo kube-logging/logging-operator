f3a12918c1b5685de9a5c1368c87e238435b23d9
Sándor Guba <gubasanyi@gmail.com>
2018-08-02 10:32:28 +0200

    crd alpha version (#16)
    * crd alpha version
    
    * Go fmt files
    
    * Minor typo fix
    
    * Fix label name
    
    * initial README
    
    * add docs
    
    * Remove ununsed s3 name tag
    
    * Remove helm charts
    
    * refactor
    
    * fix configmap name

--- pkg/stub/handler.go
+++ pkg/stub/handler.go
@@ -6,6 +6,8 @@ import (
 	"github.com/banzaicloud/logging-operator/pkg/apis/logging/v1alpha1"
 	"github.com/operator-framework/operator-sdk/pkg/sdk"
 	"github.com/sirupsen/logrus"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // NewHandler creates a new Handler struct
@@ -22,53 +24,99 @@ type Handler struct {
 func (h *Handler) Handle(ctx context.Context, event sdk.Event) (err error) {
 	switch o := event.Object.(type) {
 	case *v1alpha1.LoggingOperator:
+		if event.Deleted {
+			deleteFromConfigMap(o.Spec.Input.Label["app"])
+		}
 		logrus.Infof("New CRD arrived %#v", o)
+		logrus.Info("Generating configuration.")
+		name, config := generateFluentdConfig(o)
+		if config != "" && name != "" {
+			updateConfigMap(name, config)
+		}
 	}
 	return
 }
 
+func deleteFromConfigMap(name string) {
+	configMap := &corev1.ConfigMap{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ConfigMap",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "fluentd-app-config",
+			Namespace: "default",
+		},
+	}
+	err := sdk.Get(configMap)
+	if err != nil {
+		logrus.Error(err)
+	}
+	if configMap.Data == nil {
+		configMap.Data = map[string]string{}
+	}
+	delete(configMap.Data, name+".conf")
+	err = sdk.Update(configMap)
+	if err != nil {
+		logrus.Error(err)
+	}
+}
+
+func updateConfigMap(name, config string) {
+	configMap := &corev1.ConfigMap{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ConfigMap",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "fluentd-app-config",
+			Namespace: "default",
+		},
+	}
+	err := sdk.Get(configMap)
+	if err != nil {
+		logrus.Error(err)
+	}
+	if configMap.Data == nil {
+		configMap.Data = map[string]string{}
+	}
+	configMap.Data[name+".conf"] = config
+	err = sdk.Update(configMap)
+	if err != nil {
+		logrus.Error(err)
+	}
+}
+
 //
-//generateFluentdConfig(*v1alpha1.LoggingOperator) {
-//
-//}
+func generateFluentdConfig(crd *v1alpha1.LoggingOperator) (string, string) {
+	var finalConfig string
+	// Create pattern for match
+	baseMap := map[string]string{}
+	baseMap["pattern"] = crd.Spec.Input.Label["app"]
+	// Generate filters
+	for _, filter := range crd.Spec.Filter {
+		logrus.Info("Applying filter")
+		values := filter.GetMap()
+		values["pattern"] = crd.Spec.Input.Label["app"]
+		config, err := filter.Render()
+		if err != nil {
+			logrus.Error("Error in rendering template.")
+			return "", ""
+		}
+		finalConfig += config
+	}
 
-type loggingOperatorCRD struct {
-	Input  inputFluentd
-	Filter filterFluentd
-}
+	// Generate output
+	for _, output := range crd.Spec.Output {
+		values := output.S3.GetMap()
+		values["pattern"] = crd.Spec.Input.Label["app"]
+		config, err := output.S3.Render()
+		if err != nil {
+			logrus.Info("Error in rendering template.")
+			return "", ""
+		}
+		finalConfig += config
+	}
+	return baseMap["pattern"], finalConfig
 
-type inputFluentd struct {
-	Label string
-	Value string
-}
-
-type filterFluentd struct {
-	Name       string
-	Format     string
-	TimeFormat string
-}
-
-type outputFluentd struct {
-	S3 outputS3
-}
-
-type outputS3 struct {
-	Parameters Parameters
-}
-
-// Parameters struct
-type Parameters struct {
-	Name      string
-	ValueFrom ValueFrom
-	Value     string
-}
-
-// ValueFrom struct
-type ValueFrom struct {
-	SecretKeyRef kubernetesSecret
-}
-
-type kubernetesSecret struct {
-	Name string
-	Key  string
 }
