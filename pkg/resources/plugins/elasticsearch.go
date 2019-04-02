089bdf16028a641ba8f311d4f88ff6eb7bd09b07
tarokkk <gubasanyi@gmail.com>
2019-04-02 13:47:42 +0200

    small fixes for elasticsearch parameters

--- pkg/resources/plugins/elasticsearch.go
+++ pkg/resources/plugins/elasticsearch.go
@@ -5,15 +5,22 @@ const ElasticsearchOutput = "elasticsearch"
 
 // ElasticsearchDefaultValues for Elasticsearch output plugin
 var ElasticsearchDefaultValues = map[string]string{
-	"bufferPath":      "/buffers/elasticsearch",
-	"logLevel":        "info",
-	"logstashFormat":  "true",
-	"logstashPrefix":  "logstash",
-	"chunkLimit":      "2M",
-	"queueLimit":      "8",
-	"timekey":         "1h",
-	"timekey_wait":    "10m",
-	"timekey_use_utc": "true",
+	"bufferPath":         "/buffers/elasticsearch",
+	"logLevel":           "info",
+	"logstashFormat":     "true",
+	"logstashPrefix":     "logstash",
+	"scheme":             "scheme",
+	"chunkLimit":         "2M",
+	"queueLimit":         "8",
+	"timekey":            "1h",
+	"timekey_wait":       "10m",
+	"timekey_use_utc":    "true",
+	"retry_max_interval": "30",
+	"flush_interval":     "5s",
+	"flush_thread_count": "2",
+	"retry_forever":      "true",
+	"user":               "",
+	"password":           "",
 }
 
 // ElasticsearchTemplate for Elasticsearch output plugin
@@ -25,10 +32,16 @@ const ElasticsearchTemplate = `
   type_name fluentd
   host {{ .host }}
   port {{ .port }}
-  scheme  {{ .schema }}
+  scheme  {{ .scheme }}
   logstash_format {{ .logstashFormat }}
   logstash_prefix {{ .logstashPrefix }}
   reconnect_on_error true
+  {{- if .user }}
+  user {{ .user }}
+  {{- end}}
+  {{- if .password }}
+  password {{ .password }}
+  {{- end}}
   <buffer tag, time>
     @type file
     path {{ .bufferPath }}
@@ -37,10 +50,10 @@ const ElasticsearchTemplate = `
     timekey_use_utc {{ .timekey_use_utc }}
     flush_mode interval
     retry_type exponential_backoff
-    flush_thread_count 2
-    flush_interval 5s
-    retry_forever
-    retry_max_interval 30
+    flush_thread_count {{ .flush_thread_count }}
+    flush_interval {{ .flush_interval }}
+    retry_forever {{ .retry_forever }}
+    retry_max_interval {{ .retry_max_interval }}
     chunk_limit_size {{ .chunkLimit }}
     queue_limit_length {{ .queueLimit }}
     overflow_action block
