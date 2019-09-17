aedabc616fb6663ebe698f600d89d1ceea4714f3
Peter Szabo <asdwsda@gmail.com>
2019-09-17 15:20:48 +0200

    circleci: end-to-end test readded

---
+++ hack/test.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+set -eufo pipefail
+
+SCRIPT_PATH="$(dirname "$(readlink -f "$0")")"
+BUCKET='minio/test'
+
+function main()
+{
+    local mc_pod="$(get_mc_pod_name)"
+    create_test_bucket "${mc_pod}"
+    kubectl apply -f "${SCRIPT_PATH}/secret.yaml"
+    helm_deploy_logging_operator
+    configure_logging
+
+    wait_for_log_files "${mc_pod}" 300
+    print_logs "${mc_pod}"
+}
+
+function create_test_bucket()
+{
+    local mc_pod="$1"
+    kubectl exec --namespace logging "${mc_pod}" -- \
+        mc mb --region 'test_region' "${BUCKET}"
+}
+
+function helm_deploy_logging_operator()
+{
+    helm install \
+        --wait \
+        --name logging-operator \
+        --set image.tag='local' \
+        --set image.repository='controller' \
+        "${SCRIPT_PATH}/../charts/logging-operator"
+}
+
+function configure_logging()
+{
+    kubectl apply -f "${SCRIPT_PATH}/logging.yaml"
+    kubectl apply -f "${SCRIPT_PATH}/clusteroutput.yaml"
+    kubectl apply -f "${SCRIPT_PATH}/clusterflow.yaml"
+}
+
+function get_mc_pod_name()
+{
+    kubectl get pod --namespace logging -l app=minio-mc -o 'jsonpath={.items[0].metadata.name}'
+}
+
+function wait_for_log_files()
+{
+    local mc_pod="$1"
+    local deadline="$(( $(date +%s) + $2 ))"
+
+    echo 'Waiting for log files...'
+    while [ $(date +%s) -lt ${deadline} ]; do
+        if [ $(count_log_files "${mc_pod}") -gt 0 ]; then
+            return
+        fi
+        sleep 5
+    done
+
+    echo 'Cannot find any log files within timeout'
+    exit 1
+}
+
+function count_log_files()
+{
+    local mc_pod="$1"
+
+    get_log_files "${mc_pod}" |  wc -l
+}
+
+function get_log_files()
+{
+    local mc_pod="$1"
+
+    kubectl exec --namespace logging "${mc_pod}" -- mc find "${BUCKET}"  --name '*.gz'
+}
+
+function print_logs()
+{
+    local mc_pod="$1"
+
+    kubectl exec --namespace logging "${mc_pod}" -- mc find "${BUCKET}" --name '*.gz' -exec 'mc cat {}' | gzip -d
+}
+
+main "$@"
