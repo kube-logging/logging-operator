223a39a02711eb1c690f6640efdbc3370dde87f1
tarokkk <gubasanyi@gmail.com>
2019-10-02 13:20:04 +0200

    fix documents

--- pkg/model/filter/tagnormaliser.go
+++ pkg/model/filter/tagnormaliser.go
@@ -6,19 +6,19 @@ import (
 )
 
 // +docName:"Fluentd Plugin to re-tag based on log metadata"
-// More info at https://github.com/banzaicloud/fluent-plugin-tag-normaliser
+//More info at https://github.com/banzaicloud/fluent-plugin-tag-normaliser
 //
-// Available kubernetes metadata
+//Available kubernetes metadata
 //
-// | Parameter | Description | Example |
-// |-----------|-------------|---------|
-// | ${pod_name} | Pod name | understood-butterfly-nginx-logging-demo-7dcdcfdcd7-h7p9n |
-// | ${container_name} | Container name inside the Pod | nginx-logging-demo |
-// | ${namespace_name} | Namespace name | default |
-// | ${pod_id} | Kubernetes UUID for Pod | 1f50d309-45a6-11e9-b795-025000000001  |
-// | ${labels} | Kubernetes Pod labels. This is a nested map. You can access nested attributes via `.`  | {"app":"nginx-logging-demo", "pod-template-hash":"7dcdcfdcd7" }  |
-// | ${host} | Node hostname the Pod runs on | docker-desktop |
-// | ${docker_id} | Docker UUID of the container | 3a38148aa37aa3... |
+//| Parameter | Description | Example |
+//|-----------|-------------|---------|
+//| ${pod_name} | Pod name | understood-butterfly-nginx-logging-demo-7dcdcfdcd7-h7p9n |
+//| ${container_name} | Container name inside the Pod | nginx-logging-demo |
+//| ${namespace_name} | Namespace name | default |
+//| ${pod_id} | Kubernetes UUID for Pod | 1f50d309-45a6-11e9-b795-025000000001  |
+//| ${labels} | Kubernetes Pod labels. This is a nested map. You can access nested attributes via `.`  | {"app":"nginx-logging-demo", "pod-template-hash":"7dcdcfdcd7" }  |
+//| ${host} | Node hostname the Pod runs on | docker-desktop |
+//| ${docker_id} | Docker UUID of the container | 3a38148aa37aa3... |
 type _docTagNormaliser interface{}
 
 // +docName:"Tag Normaliser parameters"
