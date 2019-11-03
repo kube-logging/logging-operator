0f93c143e6cd882e4e7b5b3cc1551e73404059c5
Ferenc HERNADI <ferenc.hernadi@gmail.com>
2019-11-03 09:44:16 +0100

    Output tests (#204)

---
+++ pkg/model/output/kafka_test.go
@@ -0,0 +1,61 @@
+// Copyright © 2019 Banzai Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package output_test
+
+import (
+	"testing"
+
+	"github.com/banzaicloud/logging-operator/pkg/model/output"
+	"github.com/banzaicloud/logging-operator/pkg/model/render"
+	"github.com/ghodss/yaml"
+)
+
+func TestKafka(t *testing.T) {
+	CONFIG := []byte(`
+brokers: kafka-headless.kafka.svc.cluster.local:29092
+default_topic: topic
+sasl_over_ssl: false
+format:
+  type: json
+buffer:
+  timekey: 1m
+  timekey_wait: 30s
+  timekey_use_utc: true
+`)
+	expected := `
+  <match **>
+    @type kafka2
+    @id test_kafka2
+    brokers kafka-headless.kafka.svc.cluster.local:29092
+    default_topic topic
+    sasl_over_ssl false
+    <buffer tag,time>
+      @type file
+      path /buffers/test_kafka2.*.buffer
+      retry_forever true
+      timekey 1m
+      timekey_use_utc true
+      timekey_wait 30s
+    </buffer>
+    <format>
+      @type json
+    </format>
+  </match>
+`
+	kafka := &output.KafkaOutputConfig{}
+	yaml.Unmarshal(CONFIG, kafka)
+	test := render.NewOutputPluginTest(t, kafka)
+	test.DiffResult(expected)
+}
