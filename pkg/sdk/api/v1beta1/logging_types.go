3859fbe0732ee28c57904a7f2ae195dbcee1103f
Ferenc Hernadi <ferenc.hernadi@gmail.com>
2019-11-28 14:33:38 +0100

    status

--- pkg/sdk/api/v1beta1/logging_types.go
+++ pkg/sdk/api/v1beta1/logging_types.go
@@ -84,7 +84,7 @@ func (l *Logging) SetDefaults() (*Logging, error) {
 			copy.Spec.FluentdSpec.Image.Repository = "banzaicloud/fluentd"
 		}
 		if copy.Spec.FluentdSpec.Image.Tag == "" {
-			copy.Spec.FluentdSpec.Image.Tag = "v1.7.4-alpine-10"
+			copy.Spec.FluentdSpec.Image.Tag = "v1.7.4-alpine-10-dev1"
 		}
 		if copy.Spec.FluentdSpec.Image.PullPolicy == "" {
 			copy.Spec.FluentdSpec.Image.PullPolicy = "IfNotPresent"
