f7b347f755068428b8c5ec0d1ccecdf541af0f4e
Adam Graves <adam.graves85@gmail.com>
2020-02-26 23:24:31 +0000

    Fix Kinesis assume role config

--- pkg/sdk/model/output/kinesis_stream.go
+++ pkg/sdk/model/output/kinesis_stream.go
@@ -103,6 +103,12 @@ type KinesisStreamAssumeRoleCredentials struct {
 	ExternalId string `json:"external_id,omitempty"`
 }
 
+func (o *KinesisStreamAssumeRoleCredentials) ToDirective(secretLoader secret.SecretLoader, id string) (types.Directive, error) {
+	return types.NewFlatDirective(types.PluginMeta{
+		Directive: "assume_role_credentials",
+	}, o, secretLoader)
+}
+
 func (e *KinesisStreamOutputConfig) ToDirective(secretLoader secret.SecretLoader, id string) (types.Directive, error) {
 	pluginType := "kinesis_streams"
 	pluginID := id + "_" + pluginType
@@ -118,6 +124,13 @@ func (e *KinesisStreamOutputConfig) ToDirective(secretLoader secret.SecretLoader
 		return nil, err
 	} else {
 		kinesis.Params = params
+	}
+	if e.AssumeRoleCredentials != nil {
+		if assumeRoleCredentials, err := e.AssumeRoleCredentials.ToDirective(secretLoader, pluginID); err != nil {
+			return nil, err
+		} else {
+			kinesis.SubDirectives = append(kinesis.SubDirectives, assumeRoleCredentials)
+		}
 	}
 	if e.Buffer != nil {
 		if buffer, err := e.Buffer.ToDirective(secretLoader, pluginID); err != nil {
