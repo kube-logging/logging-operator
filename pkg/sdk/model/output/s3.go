e557875d25cd21758409143cfa2ce1415ee4feea
Ferenc HERNADI <ferenc.hernadi@gmail.com>
2020-05-18 15:16:19 +0200

    Release/3.2.0 rc4 (#484)
    * New RC version 3.1.0-RC4
    * New fluentd image version
    * Doc Plugin version update

--- pkg/sdk/model/output/s3.go
+++ pkg/sdk/model/output/s3.go
@@ -54,8 +54,8 @@ type _hugoS3 interface{}
 type _docS3 interface{}
 
 // +name:"Amazon S3"
-// +url:"https://github.com/fluent/fluent-plugin-s3/releases/tag/v1.3.0"
-// +version:"1.3.0"
+// +url:"https://github.com/fluent/fluent-plugin-s3/releases/tag/v1.3.1"
+// +version:"1.3.1"
 // +description:"Store logs in Amazon S3"
 // +status:"GA"
 type _metaS3 interface{}
