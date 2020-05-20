e722d71878f177461cf28a0cab2c17d9af14c3b0
Ferenc Hernadi <ferenc.hernadi@gmail.com>
2020-05-20 15:37:22 +0200

    New sdk version

--- pkg/sdk/resourcebuilder/component.go
+++ pkg/sdk/resourcebuilder/component.go
@@ -36,7 +36,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 )
 
-const Image = "banzaicloud/logging-operator:3.2.0-rc3"
+const Image = "banzaicloud/logging-operator:3.2.0"
 
 // +kubebuilder:object:generate=true
 
