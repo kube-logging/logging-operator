5e5e02409d0021b110e49bc3aaeb9a38d9cffbf6
Ferenc Hernadi <ferenc.hernadi@gmail.com>
2020-07-14 14:23:04 +0200

    plugin version docs update

--- pkg/sdk/model/output/loki.go
+++ pkg/sdk/model/output/loki.go
@@ -43,7 +43,7 @@ type _docLoki interface{}
 
 // +name:"Grafana Loki"
 // +url:"https://github.com/grafana/loki/tree/master/fluentd/fluent-plugin-grafana-loki"
-// +version:"1.2.12"
+// +version:"1.2.13"
 // +description:"Transfer logs to Loki"
 // +status:"GA"
 type _metaLoki interface{}
