90670913088108798413eec27c591d865ff04432
Ferenc HERNADI <ferenc.hernadi@gmail.com>
2020-07-14 13:28:54 +0200

    Component update 3.2.1 -> 3.4.0 (#521)

--- pkg/sdk/resourcebuilder/component.go
+++ pkg/sdk/resourcebuilder/component.go
@@ -36,7 +36,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 )
 
-const Image = "banzaicloud/logging-operator:3.2.1"
+const Image = "banzaicloud/logging-operator:3.4.0"
 
 // +kubebuilder:object:generate=true
 
