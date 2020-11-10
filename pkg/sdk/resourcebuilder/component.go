a00e3dcb9d165b93a6837556f0504a60893d275a
tarokkk <gubasanyi@gmail.com>
2020-11-10 10:01:49 +0100

    release 3.7.3

--- pkg/sdk/resourcebuilder/component.go
+++ pkg/sdk/resourcebuilder/component.go
@@ -38,7 +38,7 @@ import (
 )
 
 const (
-	Image            = "ghcr.io/banzaicloud/logging-operator:3.7.1"
+	Image            = "ghcr.io/banzaicloud/logging-operator:3.7.3"
 	defaultNamespace = "logging-system"
 )
 
