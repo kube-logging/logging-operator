f0c91cc36d43ebd90579c647265ed256eb29105d
tarokkk <gubasanyi@gmail.com>
2020-11-27 10:59:27 +0100

    change version in resourcebuilder

--- pkg/sdk/resourcebuilder/component.go
+++ pkg/sdk/resourcebuilder/component.go
@@ -38,7 +38,7 @@ import (
 )
 
 const (
-	Image            = "ghcr.io/banzaicloud/logging-operator:3.8.0"
+	Image            = "ghcr.io/banzaicloud/logging-operator:3.8.1"
 	defaultNamespace = "logging-system"
 )
 
