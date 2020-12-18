d9fb00bfec3c540bd7f0e6cfc0b786e8a95d6506
Ferenc Hernadi <ferenc.hernadi@gmail.com>
2020-12-18 10:33:30 +0100

    status

--- pkg/sdk/api/v1beta1/logging_types.go
+++ pkg/sdk/api/v1beta1/logging_types.go
@@ -63,7 +63,8 @@ type LoggingSpec struct {
 	ControlNamespace string `json:"controlNamespace"`
 	// Allow configuration of cluster resources from any namespace. Mutually exclusive with ControlNamespace restriction of Cluster resources
 	AllowClusterResourcesFromAllNamespaces bool `json:"allowClusterResourcesFromAllNamespaces,omitempty"`
-
+	// NodeAgent Configuration
+	NodeAgents              []*NodeAgent     `json:"nodeAgents,omitempty"`
 	// EnableRecreateWorkloadOnImmutableFieldChange enables the operator to recreate the
 	// fluentbit daemonset and the fluentd statefulset (and possibly other resource in the future)
 	// in case there is a change in an immutable field
