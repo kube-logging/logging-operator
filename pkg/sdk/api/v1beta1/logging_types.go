20aed1eb91216d5d4648e9712a5cdf893be8d137
pepov <peterwilcsinszky@gmail.com>
2021-05-26 14:54:03 +0200

    Fix meta registration and fix make test

--- pkg/sdk/api/v1beta1/logging_types.go
+++ pkg/sdk/api/v1beta1/logging_types.go
@@ -471,6 +471,7 @@ func init() {
 	// SchemeBuilder.Register(&Logging{}, &LoggingList{})
 	SchemeBuilder.Register(func(scheme *runtime.Scheme) error {
 		scheme.AddKnownTypes(GroupVersion, &Logging{}, &LoggingList{})
+		metav1.AddToGroupVersion(scheme, GroupVersion)
 		return nil
 	})
 }
