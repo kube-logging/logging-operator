325bfd5677afcdb3c0eeff84173faaadfd4b66ef
Sándor Lovász <39086512+bshifter@users.noreply.github.com>
2021-06-09 11:07:36 +0200

    Update pkg/sdk/api/v1alpha1/groupversion_info.go
    Co-authored-by: Dudás Ádám <adudas@cisco.com>

--- pkg/sdk/api/v1alpha1/groupversion_info.go
+++ pkg/sdk/api/v1alpha1/groupversion_info.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package v1alpha2 contains API Schema definitions for the logging v1alpha2 API group
+// Package v1alpha1 contains API Schema definitions for the logging v1alpha1 API group
 // +kubebuilder:object:generate=true
 // +groupName=logging.banzaicloud.io
 package v1alpha1
