05819efcc7bb59d5ad35267be0fd4f70e598791c
shifter <salovasz@cisco.com>
2021-06-09 11:12:49 +0200

    Use cache selectors to narrow down watched resources

--- main.go
+++ main.go
@@ -31,12 +31,16 @@ import (
 	loggingv1beta1 "github.com/banzaicloud/logging-operator/pkg/sdk/api/v1beta1"
 	"github.com/banzaicloud/logging-operator/pkg/sdk/model/types"
 	prometheusOperator "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	apiextensions "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	// +kubebuilder:scaffold:imports
@@ -61,12 +65,16 @@ func main() {
 	var enableLeaderElection bool
 	var verboseLogging bool
 	var enableprofile bool
+	var namespace string
+	var loggingRef string
 
 	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
 		"Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
 	flag.BoolVar(&verboseLogging, "verbose", false, "Enable verbose logging")
 	flag.BoolVar(&enableprofile, "pprof", false, "enable pprof")
+	flag.StringVar(&namespace, "logging-namespace", "", "logging-operator namespace")
+	flag.StringVar(&loggingRef, "logging-name", "", "logging-operator name reference")
 	flag.Parse()
 
 	ctx := context.Background()
@@ -75,14 +83,22 @@ func main() {
 		o.Development = verboseLogging
 	}))
 
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+	mgrOptions := ctrl.Options{
 		Scheme:             scheme,
 		MetricsBindAddress: metricsAddr,
 		LeaderElection:     enableLeaderElection,
 		LeaderElectionID:   "logging-operator." + loggingv1beta1.GroupVersion.Group,
 		MapperProvider:     k8sutil.NewCached,
 		Port:               9443,
-	})
+	}
+
+	customMgrOptions, err := setupCustomCache(&mgrOptions, namespace, loggingRef)
+	if err != nil {
+		setupLog.Error(err, "unable to set up custom cache settings")
+		os.Exit(1)
+	}
+
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), *customMgrOptions)
 
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
@@ -145,4 +161,42 @@ func detectContainerRuntime(ctx context.Context, c client.Reader) error {
 
 	setupLog.Info("Unable to detect container runtime, keeping default value", "runtime", types.ContainerRuntime)
 	return nil
+}
+
+func setupCustomCache(mgrOptions *ctrl.Options, namespace string, loggingRef string) (*ctrl.Options, error) {
+	if namespace == "" && loggingRef == "" {
+		return mgrOptions, nil
+	}
+
+	var namespaceSelector fields.Selector
+	var labelSelector labels.Selector
+	if namespace != "" {
+		namespaceSelector = fields.Set{"metadata.namespace": namespace}.AsSelector()
+	}
+	if loggingRef != "" {
+		labelSelector = labels.Set{"app.kubernetes.io/managed-by": loggingRef}.AsSelector()
+	}
+
+	selectorsByObject := cache.SelectorsByObject{
+		&corev1.Pod{}: {
+			Field: namespaceSelector,
+			Label: labelSelector,
+		},
+		&appsv1.DaemonSet{}: {
+			Field: namespaceSelector,
+			Label: labelSelector,
+		},
+		&appsv1.StatefulSet{}: {
+			Field: namespaceSelector,
+			Label: labelSelector,
+		},
+		&appsv1.Deployment{}: {
+			Field: namespaceSelector,
+			Label: labelSelector,
+		},
+	}
+
+	mgrOptions.NewCache = cache.BuilderWithOptions(cache.Options{SelectorsByObject: selectorsByObject})
+
+	return mgrOptions, nil
 }
