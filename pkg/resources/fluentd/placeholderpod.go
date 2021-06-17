c3d390a3e9cb6b1da42603b0c69cf118c5d7338f
Dudás Ádám <adudas@cisco.com>
2021-06-17 15:40:53 +0200

    placeholder pod while draining

---
+++ pkg/resources/fluentd/placeholderpod.go
@@ -0,0 +1,42 @@
+// Copyright © 2021 Banzai Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fluentd
+
+import (
+	"strings"
+
+	corev1 "k8s.io/api/core/v1"
+)
+
+func (r *Reconciler) placeholderPodFor(pvc corev1.PersistentVolumeClaim) *corev1.Pod {
+	tgps := int64(0)
+	return &corev1.Pod{
+		ObjectMeta: r.FluentdObjectMeta(StatefulSetName+pvc.Name[strings.LastIndex(pvc.Name, "-"):], ComponentDrainer),
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:  "pause",
+					Image: "k8s.gcr.io/pause",
+				},
+			},
+			NodeSelector:                  r.Logging.Spec.FluentdSpec.NodeSelector,
+			Tolerations:                   r.Logging.Spec.FluentdSpec.Tolerations,
+			Affinity:                      r.Logging.Spec.FluentdSpec.Affinity,
+			PriorityClassName:             r.Logging.Spec.FluentdSpec.PodPriorityClassName,
+			RestartPolicy:                 corev1.RestartPolicyNever,
+			TerminationGracePeriodSeconds: &tgps,
+		},
+	}
+}
