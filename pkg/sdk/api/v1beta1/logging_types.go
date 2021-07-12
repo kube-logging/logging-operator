67c7c3b191bcefffa7ffee15ad8ef80477c02655
Ferenc Hernadi <ferenc.hernadi@gmail.com>
2021-07-12 15:20:26 +0200

    Version bump

--- pkg/sdk/api/v1beta1/logging_types.go
+++ pkg/sdk/api/v1beta1/logging_types.go
@@ -114,7 +114,7 @@ const (
 	DefaultFluentbitImageRepository         = "fluent/fluent-bit"
 	DefaultFluentbitImageTag                = "1.7.9"
 	DefaultFluentdImageRepository           = "ghcr.io/banzaicloud/fluentd"
-	DefaultFluentdImageTag                  = "v1.12.4-alpine-1"
+	DefaultFluentdImageTag                  = "v1.12.4-alpine-3"
 	DefaultFluentdBufferStorageVolumeName   = "fluentd-buffer"
 	DefaultFluentdDrainWatchImageRepository = "ghcr.io/banzaicloud/fluentd-drain-watch"
 	DefaultFluentdDrainWatchImageTag        = "v0.0.1"
