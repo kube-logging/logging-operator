9b47c35ac85267e846d5f562b54ff1dcaf49eeb6
Ferenc Hernadi <ferenc.hernadi@gmail.com>
2021-12-13 21:35:34 +0100

    fluent-plugin-detect-exceptions permission fix

--- pkg/sdk/api/v1beta1/logging_types.go
+++ pkg/sdk/api/v1beta1/logging_types.go
@@ -116,7 +116,7 @@ const (
 	DefaultFluentbitImageRepository         = "fluent/fluent-bit"
 	DefaultFluentbitImageTag                = "1.8.9"
 	DefaultFluentdImageRepository           = "ghcr.io/banzaicloud/fluentd"
-	DefaultFluentdImageTag                  = "v1.13.3-alpine-12"
+	DefaultFluentdImageTag                  = "v1.13.3-alpine-14"
 	DefaultFluentdBufferStorageVolumeName   = "fluentd-buffer"
 	DefaultFluentdDrainWatchImageRepository = "ghcr.io/banzaicloud/fluentd-drain-watch"
 	DefaultFluentdDrainWatchImageTag        = "v0.0.1"
