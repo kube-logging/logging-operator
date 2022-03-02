f22ee78fd1d52355cc00c8f21aeaeee8e55dc3d6
Samuele Chiocca <samuele@nutellino.it>
2022-03-02 12:22:18 +0100

    Add Skip_Long_Lines to Filetailer Hosttailer
    This parameter, On by default, will skip the lines that exceed the Buffer_Max_Size parameter
    
    Co-Authored-By: Reuenthal767 <45149083+Reuenthal767@users.noreply.github.com>

--- pkg/sdk/extensions/api/v1alpha1/hosttailer_types.go
+++ pkg/sdk/extensions/api/v1alpha1/hosttailer_types.go
@@ -80,8 +80,10 @@ type FileTailer struct {
 	Path string `json:"path,omitempty"`
 	// Disable tailing the file
 	Disabled bool `json:"disabled,omitempty"`
-	// Set the limit of the buffer size per monitored file (e.g: very long lines)
+	// Set the limit of the buffer size per monitored file
 	BufferMaxSize string `json:"buffer_max_size,omitempty"`
+	// Skip long line when exceeding Buffer_Max_Size
+	SkipLongLines string `json:"skip_long_lines,omitempty"`
 	// Override container fields for the given tailer
 	ContainerBase *types.ContainerBase `json:"containerOverrides,omitempty"`
 }
