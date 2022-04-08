1482b745102b9cf67f776bedb0b09159989bca8a
Ferenc Hernadi <ferenc.hernadi@gmail.com>
2022-04-08 10:15:25 +0200

    Tailer ChunkSize parameter support

--- pkg/sdk/extensions/api/v1alpha1/hosttailer_types.go
+++ pkg/sdk/extensions/api/v1alpha1/hosttailer_types.go
@@ -80,8 +80,10 @@ type FileTailer struct {
 	Path string `json:"path,omitempty"`
 	// Disable tailing the file
 	Disabled bool `json:"disabled,omitempty"`
-	// Set the limit of the buffer size per monitored file
+	// Set the limit of the buffer size per active filetailer
 	BufferMaxSize string `json:"buffer_max_size,omitempty"`
+	// Set the buffer chunk size per active filetailer
+	BufferChunkSize string `json:"buffer_chunk_size,omitempty"`
 	// Skip long line when exceeding Buffer_Max_Size
 	SkipLongLines string `json:"skip_long_lines,omitempty"`
 	// Override container fields for the given tailer
