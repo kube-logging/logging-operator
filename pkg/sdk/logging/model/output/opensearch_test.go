9ef3d4b27b593a7755b20344dc39685bb0ea5247
Ferenc Hernadi <ferenc.hernadi@gmail.com>
2022-04-16 23:30:26 +0200

    OpenSearch Support

---
+++ pkg/sdk/logging/model/output/opensearch_test.go
@@ -0,0 +1,71 @@
+// Copyright © 2022 Banzai Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package output_test
+
+import (
+	"testing"
+
+	"github.com/banzaicloud/logging-operator/pkg/sdk/logging/model/output"
+	"github.com/banzaicloud/logging-operator/pkg/sdk/logging/model/render"
+	"github.com/ghodss/yaml"
+	"github.com/stretchr/testify/require"
+)
+
+func TestOpenSearch(t *testing.T) {
+	CONFIG := []byte(`
+host: opensearch-cluster.default.svc.cluster.local
+port: 9200
+scheme: https
+ssl_version: TLSv1_2
+ssl_verify: false
+buffer:
+  timekey: 1m
+  timekey_wait: 30s
+  timekey_use_utc: true
+`)
+	expected := `
+  <match **>
+	@type opensearch
+	@id test
+    catch_transport_exception_on_retry true
+    emit_error_label_event true
+    exception_backup true
+    fail_on_detecting_os_version_retry_exceed true
+    fail_on_putting_template_retry_exceed true
+	host opensearch-cluster.default.svc.cluster.local
+    http_backend_excon_nonblock true
+	port 9200
+	reload_connections true
+	scheme https
+	ssl_verify false
+    use_legacy_template true
+	utc_index true
+    verify_os_version_at_startup true
+    <buffer tag,time>
+      @type file
+	  chunk_limit_size 8MB
+      path /buffers/test.*.buffer
+      retry_forever true
+      timekey 1m
+      timekey_use_utc true
+      timekey_wait 30s
+    </buffer>
+  </match>
+`
+	es := &output.OpenSearchOutput{}
+	require.NoError(t, yaml.Unmarshal(CONFIG, es))
+	test := render.NewOutputPluginTest(t, es)
+	test.DiffResult(expected)
+}
