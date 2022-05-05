4a7f044cc4a0bd6a85bdc8f5a2de54cfc1274e35
Ferenc Hernadi <ferenc.hernadi@gmail.com>
2022-05-05 22:24:15 +0200

    Auth Lib extension

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ import (
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/klog/v2"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
