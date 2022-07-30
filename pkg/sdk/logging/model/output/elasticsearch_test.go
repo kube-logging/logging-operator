0d704ca9390345f0190a66efa5f264982b9721d2
Petr Široký <petr.siroky@pm.me>
2022-07-30 21:53:48 +0200

    Fix Fluentd warning "parameter 'data_stream_enable' is not used"
     * attribute 'data_stream_enable' is artificial / only used
       by the logging operator. It is not something Fluentd knows about
       as the data stream usage is denoted by different @type.
       It does not really break anything, but Fluentd prints ugly
       warning for each output using data stream.

--- pkg/sdk/logging/model/output/elasticsearch_test.go
+++ pkg/sdk/logging/model/output/elasticsearch_test.go
@@ -66,3 +66,50 @@ buffer:
 	test := render.NewOutputPluginTest(t, es)
 	test.DiffResult(expected)
 }
+
+func TestElasticSearchDataStream(t *testing.T) {
+	CONFIG := []byte(`
+host: elasticsearch-elasticsearch-cluster.default.svc.cluster.local
+port: 9200
+scheme: https
+ssl_version: TLSv1_2
+ssl_verify: false
+data_stream_enable: true
+data_stream_name: test-ds
+buffer:
+  timekey: 1m
+  timekey_wait: 30s
+  timekey_use_utc: true
+
+`)
+	expected := `
+  <match **>
+    @type elasticsearch_data_stream
+    @id test
+    data_stream_name test-ds
+    exception_backup true
+    fail_on_putting_template_retry_exceed true
+    host elasticsearch-elasticsearch-cluster.default.svc.cluster.local
+    port 9200
+    reload_connections true
+    scheme https
+    ssl_verify false
+    ssl_version TLSv1_2
+    utc_index true
+    verify_es_version_at_startup true
+    <buffer tag,time>
+      @type file
+      chunk_limit_size 8MB
+      path /buffers/test.*.buffer
+      retry_forever true
+      timekey 1m
+      timekey_use_utc true
+      timekey_wait 30s
+    </buffer>
+  </match>
+`
+	es := &output.ElasticsearchOutput{}
+	require.NoError(t, yaml.Unmarshal(CONFIG, es))
+	test := render.NewOutputPluginTest(t, es)
+	test.DiffResult(expected)
+}
