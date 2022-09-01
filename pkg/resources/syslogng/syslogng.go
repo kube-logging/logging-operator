4cd68b772c414b646bc7bb9e8f246075f5eedd70
tarokkk <gubasanyi@gmail.com>
2022-09-01 13:28:25 +0200

    update default syslog-ng image

--- pkg/resources/syslogng/syslogng.go
+++ pkg/resources/syslogng/syslogng.go
@@ -50,8 +50,8 @@ const (
 	clusterRoleName                   = "syslog-ng"
 	containerName                     = "syslog-ng"
 	defaultBufferVolumeMetricsPort    = 9200
-	syslogngImageRepository           = "balabit/syslog-ng"
-	syslogngImageTag                  = "3.37.1"
+	syslogngImageRepository           = "ghcr.io/banzaicloud/syslog-ng"
+	syslogngImageTag                  = "3.38.2"
 	prometheusExporterImageRepository = "ghcr.io/banzaicloud/syslog-ng-exporter"
 	prometheusExporterImageTag        = "0.0.13"
 	bufferVolumeImageRepository       = "ghcr.io/banzaicloud/custom-runner"
