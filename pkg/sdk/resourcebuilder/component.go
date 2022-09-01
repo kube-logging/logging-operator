a50fcc5e0ddd58c8e7a5aff1485d2171d89d3561
Ferenc Hernadi <ferenc.hernadi@gmail.com>
2022-09-01 14:06:10 +0200

    Release 3.17.9

--- pkg/sdk/resourcebuilder/component.go
+++ pkg/sdk/resourcebuilder/component.go
@@ -50,7 +50,7 @@ import (
 )
 
 const (
-	Image            = "ghcr.io/banzaicloud/logging-operator:3.17.8"
+	Image            = "ghcr.io/banzaicloud/logging-operator:3.17.9"
 	defaultNamespace = "logging-system"
 )
 
