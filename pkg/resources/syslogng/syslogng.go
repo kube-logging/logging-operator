2640fa489ee721417bd4b018408b97449a37c1ef
Peter Wilcsinszky <peter.wilcsinszky@axoflow.com>
2023-04-12 09:27:29 +0200

    chore(deps): bump syslogng-reload
    Signed-off-by: Peter Wilcsinszky <peter.wilcsinszky@axoflow.com>

--- pkg/resources/syslogng/syslogng.go
+++ pkg/resources/syslogng/syslogng.go
@@ -58,7 +58,7 @@ const (
 	bufferVolumeImageRepository       = "ghcr.io/kube-logging/node-exporter"
 	bufferVolumeImageTag              = "v0.4.0"
 	configReloaderImageRepository     = "ghcr.io/kube-logging/syslogng-reload"
-	configReloaderImageTag            = "v1.1.0"
+	configReloaderImageTag            = "v1.2.0"
 	socketVolumeName                  = "socket"
 	socketPath                        = "/tmp/syslog-ng/syslog-ng.ctl"
 	configDir                         = "/etc/syslog-ng/config"
