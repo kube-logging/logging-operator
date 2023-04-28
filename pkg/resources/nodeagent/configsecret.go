bb15d74a13a671162ba60d936878825f25cf16d5
Peter Wilcsinszky <peter.wilcsinszky@axoflow.com>
2023-04-28 11:39:17 +0200

    chore: fix loggingdataprovider rename
    Signed-off-by: Peter Wilcsinszky <peter.wilcsinszky@axoflow.com>

--- pkg/resources/nodeagent/configsecret.go
+++ pkg/resources/nodeagent/configsecret.go
@@ -28,6 +28,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	"github.com/kube-logging/logging-operator/pkg/resources/fluentd"
+	"github.com/kube-logging/logging-operator/pkg/resources/syslogng"
 	"github.com/kube-logging/logging-operator/pkg/sdk/logging/model/types"
 )
 
@@ -85,6 +86,15 @@ type fluentBitConfig struct {
 	Upstream       struct {
 		Enabled bool
 		Config  upstream
+	}
+}
+
+type syslogNGConfig struct {
+	TargetHost string
+	TargetPort int32
+	TLS        struct {
+		Enabled   bool
+		SharedKey string
 	}
 }
 
@@ -241,11 +251,11 @@ func (n *nodeAgentInstance) configSecret() (runtime.Object, reconciler.DesiredSt
 			}
 		}
 
-		if nil == n.fluentdDataProvider {
+		if nil == n.loggingDataProvider {
 			return nil, nil, errors.WrapIf(err, "nil fluent data provider")
 		}
 
-		fluentdReplicas, err := n.fluentdDataProvider.GetReplicaCount(context.TODO(), n.logging)
+		fluentdReplicas, err := n.loggingDataProvider.GetReplicaCount(context.TODO())
 		if err != nil {
 			return nil, nil, errors.WrapIf(err, "getting replica count for fluentd")
 		}
@@ -288,205 +298,29 @@ func (n *nodeAgentInstance) configSecret() (runtime.Object, reconciler.DesiredSt
 		n.configs = confs
 
 		return &corev1.Secret{
-			ObjectMeta: n.NodeAgentObjectMeta(fluentBitSecretConfigName),
-		}, reconciler.StateAbsent, nil
-	}
-	monitor := struct {
-		Enabled bool
-		Port    int32
-		Path    string
-	}{}
-	if n.nodeAgent.FluentbitSpec.Metrics != nil {
-		monitor.Enabled = true
-		monitor.Port = n.nodeAgent.FluentbitSpec.Metrics.Port
-		monitor.Path = n.nodeAgent.FluentbitSpec.Metrics.Path
-	}
-
-	if n.nodeAgent.FluentbitSpec.InputTail.Parser == "" {
-		switch types.ContainerRuntime {
-		case "docker":
-			n.nodeAgent.FluentbitSpec.InputTail.Parser = "docker"
-		case "containerd":
-			n.nodeAgent.FluentbitSpec.InputTail.Parser = "cri"
-		default:
-			n.nodeAgent.FluentbitSpec.InputTail.Parser = "cri"
+			ObjectMeta: n.NodeAgentObjectMeta(SecretConfigNameFluentbit),
+			Data:       confs,
+		}, reconciler.StatePresent, nil
+	} else if n.nodeAgent.SyslogNGSpec != nil {
+		input := syslogNGConfig{
+			TargetHost: fmt.Sprintf("%s.%s.svc%s", n.AggregatorQualifiedName(syslogng.ServiceName), n.logging.Spec.ControlNamespace, n.logging.ClusterDomainAsSuffix()),
 		}
-	}
-
-	mapper := types.NewStructToStringMapper(nil)
-
-	// FluentBit input Values
-	fluentbitInput := fluentbitInputConfig{}
-	inputTail := n.nodeAgent.FluentbitSpec.InputTail
-
-	if len(inputTail.MultilineParser) > 0 {
-		fluentbitInput.MultilineParser = inputTail.MultilineParser
-		inputTail.MultilineParser = nil
-
-		// If MultilineParser is set, remove other parser fields
-		// See https://docs.fluentbit.io/manual/pipeline/inputs/tail#multiline-core-v1.8
-
-		log.Log.Info("Notice: MultilineParser is enabled. Disabling other parser options")
-		inputTail.Parser = ""
-		inputTail.ParserFirstline = ""
-		inputTail.ParserN = nil
-		inputTail.Multiline = ""
-		inputTail.MultilineFlush = ""
-		inputTail.DockerMode = ""
-		inputTail.DockerModeFlush = ""
-		inputTail.DockerModeParser = ""
-
-	} else if len(inputTail.ParserN) > 0 {
-		fluentbitInput.ParserN = inputTail.ParserN
-		inputTail.ParserN = nil
-	}
-
-	fluentbitInputValues, err := mapper.StringsMap(inputTail)
-	if err != nil {
-		return nil, reconciler.StatePresent, errors.WrapIf(err, "failed to map container tailer config for fluentbit")
-	}
-	fluentbitInput.Values = fluentbitInputValues
-
-	disableKubernetesFilter := n.nodeAgent.FluentbitSpec.DisableKubernetesFilter != nil && *n.nodeAgent.FluentbitSpec.DisableKubernetesFilter == true
-
-	if !disableKubernetesFilter {
-		if n.nodeAgent.FluentbitSpec.FilterKubernetes.BufferSize == "" {
-			log.Log.Info("Notice: If the Buffer_Size value is empty we will set it 0. For more information: https://github.com/fluent/fluent-bit/issues/2111")
-			n.nodeAgent.FluentbitSpec.FilterKubernetes.BufferSize = "0"
-		} else if n.nodeAgent.FluentbitSpec.FilterKubernetes.BufferSize != "0" {
-			log.Log.Info("Notice: If the kubernetes filter buffer_size parameter is underestimated it can cause log loss. For more information: https://github.com/fluent/fluent-bit/issues/2111")
-		}
-	}
-
-	fluentbitKubernetesFilter, err := mapper.StringsMap(n.nodeAgent.FluentbitSpec.FilterKubernetes)
-	if err != nil {
-		return nil, reconciler.StatePresent, errors.WrapIf(err, "failed to map kubernetes filter for fluentbit")
-	}
-
-	fluentbitBufferStorage, err := mapper.StringsMap(n.nodeAgent.FluentbitSpec.BufferStorage)
-	if err != nil {
-		return nil, reconciler.StatePresent, errors.WrapIf(err, "failed to map buffer storage for fluentbit")
-	}
-
-	input := fluentBitConfig{
-		Flush:                   n.nodeAgent.FluentbitSpec.Flush,
-		Grace:                   n.nodeAgent.FluentbitSpec.Grace,
-		LogLevel:                n.nodeAgent.FluentbitSpec.LogLevel,
-		CoroStackSize:           n.nodeAgent.FluentbitSpec.CoroStackSize,
-		Namespace:               n.logging.Spec.ControlNamespace,
-		Monitor:                 monitor,
-		TargetHost:              fmt.Sprintf("%s.%s.svc%s", n.FluentdQualifiedName(fluentd.ServiceName), n.logging.Spec.ControlNamespace, n.logging.ClusterDomainAsSuffix()),
-		Input:                   fluentbitInput,
-		DisableKubernetesFilter: disableKubernetesFilter,
-		KubernetesFilter:        fluentbitKubernetesFilter,
-		BufferStorage:           fluentbitBufferStorage,
-	}
-
-	if n.nodeAgent.FluentbitSpec != nil && n.nodeAgent.FluentbitSpec.TLS != nil {
-		input.TLS = struct {
-			Enabled   bool
-			SharedKey string
-		}{
-			Enabled:   *n.nodeAgent.FluentbitSpec.TLS.Enabled,
-			SharedKey: n.nodeAgent.FluentbitSpec.TLS.SharedKey,
-		}
-	}
-	if n.logging.Spec.FluentdSpec != nil {
-		input.TargetPort = n.logging.Spec.FluentdSpec.Port
-	}
-	if n.nodeAgent.FluentbitSpec.FilterAws != nil {
-		awsFilter, err := mapper.StringsMap(n.nodeAgent.FluentbitSpec.FilterAws)
+		conf, err := generateSyslogNGConfig(input)
 		if err != nil {
-			return nil, reconciler.StatePresent, errors.WrapIf(err, "failed to map aws filter for fluentbit")
+			return nil, reconciler.StatePresent, errors.WrapIf(err, "failed to generate config for fluentbit")
 		}
-		input.AwsFilter = awsFilter
-	}
-	if n.nodeAgent.FluentbitSpec.TargetHost != "" {
-		input.TargetHost = n.nodeAgent.FluentbitSpec.TargetHost
-	}
-	if n.nodeAgent.FluentbitSpec.TargetPort != 0 {
-		input.TargetPort = n.nodeAgent.FluentbitSpec.TargetPort
-	}
-	if n.nodeAgent.FluentbitSpec.ForwardOptions != nil {
-		forwardOptions, err := mapper.StringsMap(n.nodeAgent.FluentbitSpec.ForwardOptions)
-		if err != nil {
-			return nil, reconciler.StatePresent, errors.WrapIf(err, "failed to map forwardOptions for fluentbit")
+		confs := map[string][]byte{
+			BaseConfigNameSyslogNG: []byte(conf),
 		}
-		input.ForwardOptions = forwardOptions
+		n.configs = confs
+
+		return &corev1.Secret{
+			ObjectMeta: n.NodeAgentObjectMeta(secretConfigNameSyslogNG),
+			Data:       confs,
+		}, reconciler.StatePresent, nil
 	}
 
-	if n.nodeAgent.FluentbitSpec.Network != nil {
-		if n.nodeAgent.FluentbitSpec.Network.ConnectTimeout != nil {
-			input.Network.ConnectTimeoutSet = true
-			input.Network.ConnectTimeout = *n.nodeAgent.FluentbitSpec.Network.ConnectTimeout
-		}
-
-		if n.nodeAgent.FluentbitSpec.Network.Keepalive != nil {
-			input.Network.KeepaliveSet = true
-			input.Network.Keepalive = *n.nodeAgent.FluentbitSpec.Network.Keepalive
-		}
-
-		if n.nodeAgent.FluentbitSpec.Network.KeepaliveIdleTimeout != nil {
-			input.Network.KeepaliveIdleTimeoutSet = true
-			input.Network.KeepaliveIdleTimeout = *n.nodeAgent.FluentbitSpec.Network.KeepaliveIdleTimeout
-		}
-
-		if n.nodeAgent.FluentbitSpec.Network.KeepaliveMaxRecycle != nil {
-			input.Network.KeepaliveMaxRecycleSet = true
-			input.Network.KeepaliveMaxRecycle = *n.nodeAgent.FluentbitSpec.Network.KeepaliveMaxRecycle
-		}
-	}
-
-	if nil == n.loggingDataProvider {
-		return nil, nil, errors.WrapIf(err, "nil fluent data provider")
-	}
-
-	fluentdReplicas, err := n.loggingDataProvider.GetReplicaCount(context.TODO())
-	if err != nil {
-		return nil, nil, errors.WrapIf(err, "getting replica count for fluentd")
-	}
-
-	if n.nodeAgent.FluentbitSpec.Network == nil && utils.PointerToInt32(fluentdReplicas) > 1 {
-		input.Network.KeepaliveSet = true
-		input.Network.Keepalive = true
-		input.Network.KeepaliveIdleTimeoutSet = true
-		input.Network.KeepaliveIdleTimeout = 30
-		input.Network.KeepaliveMaxRecycleSet = true
-		input.Network.KeepaliveMaxRecycle = 100
-		log.Log.Info("Notice: Because the Fluentd statefulset has been scaled, we've made some changes in the fluentbit network config too. We advice to revise these default configurations.")
-	}
-
-	if utils.PointerToBool(n.nodeAgent.FluentbitSpec.EnableUpstream) {
-		input.Upstream.Enabled = true
-		input.Upstream.Config.Name = "fluentd-upstream"
-
-		for i := int32(0); i < utils.PointerToInt32(fluentdReplicas); i++ {
-			input.Upstream.Config.Nodes = append(input.Upstream.Config.Nodes, n.generateUpstreamNode(i))
-		}
-	}
-
-	conf, err := generateConfig(input)
-	if err != nil {
-		return nil, reconciler.StatePresent, errors.WrapIf(err, "failed to generate config for fluentbit")
-	}
-	confs := map[string][]byte{
-		BaseConfigName: []byte(conf),
-	}
-
-	if input.Upstream.Enabled {
-		upstreamConfig, err := generateUpstreamConfig(input)
-		if err != nil {
-			return nil, reconciler.StatePresent, errors.WrapIf(err, "failed to generate upstream config for fluentbit")
-		}
-		confs[UpstreamConfigName] = []byte(upstreamConfig)
-	}
-
-	n.configs = confs
-
-	return &corev1.Secret{
-		ObjectMeta: n.NodeAgentObjectMeta(fluentBitSecretConfigName),
-		Data:       confs,
-	}, reconciler.StatePresent, nil
+	return nil, reconciler.StatePresent, errors.New("not implemented")
 }
 
 func generateConfig(input fluentBitConfig) (string, error) {
@@ -503,9 +337,23 @@ func generateConfig(input fluentBitConfig) (string, error) {
 	return outputString, nil
 }
 
+func generateSyslogNGConfig(input syslogNGConfig) (string, error) {
+	output := new(bytes.Buffer)
+	tmpl, err := template.New("test").Parse(syslogNGConfigTemplate)
+	if err != nil {
+		return "", err
+	}
+	err = tmpl.Execute(output, input)
+	if err != nil {
+		return "", err
+	}
+	outputString := output.String()
+	return outputString, nil
+}
+
 func generateUpstreamConfig(input fluentBitConfig) (string, error) {
 	output := new(bytes.Buffer)
-	tmpl, err := template.New("upstream").Parse(upstreamConfigTemplate)
+	tmpl, err := template.New("upstream").Parse(fluentBitUpstreamConfigTemplate)
 	if err != nil {
 		return "", err
 	}
@@ -517,12 +365,12 @@ func generateUpstreamConfig(input fluentBitConfig) (string, error) {
 }
 
 func (n *nodeAgentInstance) generateUpstreamNode(index int32) upstreamNode {
-	podName := n.FluentdQualifiedName(fmt.Sprintf("%s-%d", fluentd.ComponentFluentd, index))
+	podName := n.AggregatorQualifiedName(fmt.Sprintf("%s-%d", fluentd.ComponentFluentd, index))
 	return upstreamNode{
 		Name: podName,
 		Host: fmt.Sprintf("%s.%s.%s.svc%s",
 			podName,
-			n.FluentdQualifiedName(fluentd.ServiceName+"-headless"),
+			n.AggregatorQualifiedName(fluentd.ServiceName+"-headless"),
 			n.logging.Spec.ControlNamespace,
 			n.logging.ClusterDomainAsSuffix()),
 		Port: 24240,
