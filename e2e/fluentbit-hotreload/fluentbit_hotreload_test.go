02849a881d7d71d8039628091da7346824b138ab
Peter Wilcsinszky <peter.wilcsinszky@axoflow.com>
2024-01-24 12:35:07 +0100

    fix license
    Signed-off-by: Peter Wilcsinszky <peter.wilcsinszky@axoflow.com>

--- e2e/fluentbit-hotreload/fluentbit_hotreload_test.go
+++ e2e/fluentbit-hotreload/fluentbit_hotreload_test.go
@@ -1,4 +1,4 @@
-// Copyright © 2021 Cisco Systems, Inc. and/or its affiliates
+// Copyright © 2024 Kube logging authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
