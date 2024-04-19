c4b98a5e5218bb1296f22650de691c5c23d6975e
László Várady <laszlo.varady@axoflow.com>
2024-04-19 17:25:43 +0200

    chore(pkg): update AxoSyslog image to 4.7.1
    Signed-off-by: László Várady <laszlo.varady@axoflow.com>

--- pkg/resources/syslogng/syslogng.go
+++ pkg/resources/syslogng/syslogng.go
@@ -52,7 +52,7 @@ const (
 	ContainerName                     = "syslog-ng"
 	defaultBufferVolumeMetricsPort    = 9200
 	syslogngImageRepository           = "ghcr.io/axoflow/axosyslog"
-	syslogngImageTag                  = "4.7.0"
+	syslogngImageTag                  = "4.7.1"
 	prometheusExporterImageRepository = "ghcr.io/axoflow/axosyslog-metrics-exporter"
 	prometheusExporterImageTag        = "0.0.2"
 	bufferVolumeImageRepository       = "ghcr.io/kube-logging/node-exporter"
