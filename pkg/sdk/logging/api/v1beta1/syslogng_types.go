328a48579f443270447edcb018fb2b991f9667a0
László Várady <laszlo.varady@axoflow.com>
2025-02-13 18:39:30 +0100

    chore(pkg): update AxoSyslog image to 4.10.1
    Signed-off-by: László Várady <laszlo.varady@axoflow.com>

--- pkg/sdk/logging/api/v1beta1/syslogng_types.go
+++ pkg/sdk/logging/api/v1beta1/syslogng_types.go
@@ -34,7 +34,7 @@ type _metaSyslogNGSpec interface{} //nolint:deadcode,unused
 
 const (
 	defaultSyslogngImageRepository    = "ghcr.io/axoflow/axosyslog"
-	defaultSyslogngImageTag           = "4.10.0"
+	defaultSyslogngImageTag           = "4.10.1"
 	configReloaderImageRepository     = "ghcr.io/kube-logging/syslogng-reload"
 	configReloaderImageTag            = "v1.6.0"
 	prometheusExporterImageRepository = "ghcr.io/axoflow/axosyslog-metrics-exporter"
