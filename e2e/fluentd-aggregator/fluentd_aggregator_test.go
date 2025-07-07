94c53c925d8fd02ed7423c0bcfcb2c539fe67838
Jose Antonio Cortes <joseacl@gmail.com>
2025-07-07 12:51:55 +0200

    Fixed syntax in e2e test
    Signed-off-by: Jose Antonio Cortes <joseacl@gmail.com>

--- e2e/fluentd-aggregator/fluentd_aggregator_test.go
+++ e2e/fluentd-aggregator/fluentd_aggregator_test.go
@@ -327,7 +327,7 @@ func TestFluentdAggregator_ConfigChecks(t *testing.T) {
 					Workers: 1,
 					Security: &v1beta1.Security{
 						SecurityContext: &corev1.SecurityContext{
-							ReadOnlyRootFilesystem:   true,
+							ReadOnlyRootFilesystem: utils.BoolPointer(true),
 						},
 					},
 				},
