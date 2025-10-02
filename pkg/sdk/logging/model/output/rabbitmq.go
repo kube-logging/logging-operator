1ce85fcdb43ac39800788c5841f08ddd5e2e8b9d
Chanakya Pallapolu <6578220+cpallapolu@users.noreply.github.com>
2025-10-02 09:28:24 -0400

    Update pkg/sdk/logging/model/output/rabbitmq.go
    Co-authored-by: Bence Csati <113284287+csatib02@users.noreply.github.com>
    Signed-off-by: Chanakya Pallapolu <6578220+cpallapolu@users.noreply.github.com>

--- pkg/sdk/logging/model/output/rabbitmq.go
+++ pkg/sdk/logging/model/output/rabbitmq.go
@@ -1,4 +1,4 @@
-// Copyright © 2020 Banzai Cloud
+// Copyright © 2025 Kube logging authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
