2f7922647230d17400804e22b73e535ae1ee301d
Bence Csati <bence.csati@axoflow.com>
2026-01-05 16:50:15 +0100

    chore: bump fluentbit to 4.2.2
    Signed-off-by: Bence Csati <bence.csati@axoflow.com>

--- pkg/sdk/extensions/extensionsconfig/config.go
+++ pkg/sdk/extensions/extensionsconfig/config.go
@@ -71,7 +71,7 @@ var Global = GlobalConfig{
 
 // HostTailer configuration
 var HostTailer = HostTailerConfig{
-	FluentBitImage: "ghcr.io/fluent/fluent-bit:4.2.1",
+	FluentBitImage: "ghcr.io/fluent/fluent-bit:4.2.2",
 	TailerAffix:    "host-tailer",
 }
 
@@ -85,7 +85,7 @@ var EventTailer = EventTailerConfig{
 
 // TailerWebhook configuration
 var TailerWebhook = TailerWebhookConfig{
-	FluentBitImage:    "ghcr.io/fluent/fluent-bit:4.2.1",
+	FluentBitImage:    "ghcr.io/fluent/fluent-bit:4.2.2",
 	AnnotationKey:     "sidecar.logging-extensions.banzaicloud.io/tail",
 	ServerPath:        "/tailer-webhook",
 	ServerPort:        9443,
