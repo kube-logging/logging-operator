8205b715b3d7cfc8ffed1e794b4950ac94409ee8
Bence Csati <bence.csati@axoflow.com>
2026-01-05 16:48:44 +0100

    chore: bump axosyslog-metrics-exporter to 0.0.15
    Signed-off-by: Bence Csati <bence.csati@axoflow.com>

--- pkg/sdk/logging/api/v1beta1/syslogng_types.go
+++ pkg/sdk/logging/api/v1beta1/syslogng_types.go
@@ -36,7 +36,7 @@ const (
 	defaultSyslogngImageRepository           = "ghcr.io/axoflow/axosyslog"
 	defaultSyslogngImageTag                  = "4.21.0"
 	defaultPrometheusExporterImageRepository = "ghcr.io/axoflow/axosyslog-metrics-exporter"
-	defaultPrometheusExporterImageTag        = "0.0.14"
+	defaultPrometheusExporterImageTag        = "0.0.15"
 	defaultConfigReloaderImageRepository     = "ghcr.io/kube-logging/logging-operator/syslog-ng-reloader"
 	defaultConfigReloaderImageTag            = "latest"
 	defaultBufferVolumeImageRepository       = "ghcr.io/kube-logging/logging-operator/node-exporter"
