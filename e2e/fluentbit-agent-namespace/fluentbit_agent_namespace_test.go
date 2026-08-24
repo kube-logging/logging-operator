9abb548b1c136191795f07369cfb230a76f049c9
Vyncint Ng <115854244+vyncint@users.noreply.github.com>
2026-08-24 20:34:02 +0700

    refactor(e2e): move fluentbit-agent-namespace onto the harness
    Signed-off-by: Vyncint Ng <115854244+vyncint@users.noreply.github.com>

--- e2e/fluentbit-agent-namespace/fluentbit_agent_namespace_test.go
+++ e2e/fluentbit-agent-namespace/fluentbit_agent_namespace_test.go
@@ -15,242 +15,109 @@
 package fluentbit_agent_namespace
 
 import (
-	"context"
-	"fmt"
-	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
 	"testing"
-	"time"
 
-	"github.com/cisco-open/operator-tools/pkg/types"
-	"github.com/stretchr/testify/require"
-	appsv1 "k8s.io/api/apps/v1"
-	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
-	rbacv1 "k8s.io/api/rbac/v1"
-	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-	apiresource "k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/cluster"
 
-	"github.com/kube-logging/logging-operator/e2e/common"
-	"github.com/kube-logging/logging-operator/e2e/common/setup"
 	"github.com/kube-logging/logging-operator/e2e/internal/harness"
 	"github.com/kube-logging/logging-operator/e2e/internal/image"
 	"github.com/kube-logging/logging-operator/e2e/internal/wait"
-	v1beta1 "github.com/kube-logging/logging-operator/pkg/sdk/logging/api/v1beta1"
+	"github.com/kube-logging/logging-operator/pkg/sdk/logging/api/v1beta1"
 	"github.com/kube-logging/logging-operator/pkg/sdk/logging/model/output"
 )
 
-var TestTempDir string
-
-func init() {
-	var ok bool
-	TestTempDir, ok = os.LookupEnv("PROJECT_DIR")
-	if !ok {
-		TestTempDir = "../.."
-	}
-	TestTempDir = filepath.Join(TestTempDir, "build/_test")
-	err := os.MkdirAll(TestTempDir, os.FileMode(0o755))
-	if err != nil {
-		panic(err)
-	}
-}
-
-var (
-	tags           = "time"
-	realTimeBuffer = &output.Buffer{
-		Tags:        &tags,
-		Timekey:     "1s",
-		TimekeyWait: "0s",
-	}
+const (
+	release    = "fluentbit-agents-namespace"
+	nsControl  = "logging"
+	nsAgents   = "logging-fluentbit-agents"
+	nsProducer = "default"
+	tag        = "tag_fluentbit_agents"
 )
 
-var producerLabels = map[string]string{
-	"my-unique-label": "log-producer",
+var producerLabels = map[string]string{"my-unique-label": "log-producer"}
+
+func realTimeBuffer() *output.Buffer {
+	tags := "time"
+	return &output.Buffer{Tags: &tags, Timekey: "1s", TimekeyWait: "0s"}
 }
 
 // Verifies that Fluentbit agents are deployed to a dedicated namespace when
-// logging.spec.fluentBitAgentNamespace is set, while the aggregator stays in the control namespace.
+// logging.spec.fluentBitAgentNamespace is set, while the aggregator stays in
+// the control namespace.
 func TestFluentbitAgentDedicatedNamespace(t *testing.T) {
-	common.Initialize(t)
+	env := harness.New(t).
+		WithCluster(release).
+		WithRelease(release).
+		WithControlNamespace(nsControl).
+		WithNamespaces(nsAgents).
+		Start()
 
-	nsControl := "logging"
-	nsAgents := "logging-fluentbit-agents"
-	release := "fluentbit-agents-namespace"
-	tag := "tag_fluentbit_agents"
-
-	common.WithCluster(release, t, func(t *testing.T, c common.Cluster) {
-		// Install logging-operator Helm chart with test-receiver in control namespace
-		setup.LoggingOperator(t, c, setup.LoggingOperatorOptionFunc(func(options *setup.LoggingOperatorOptions) {
-			options.Namespace = nsControl
-			options.NameOverride = release
-		}))
-
-		ctx := context.Background()
-
-		// Ensure fluentbit agent namespace exists
-		common.RequireNoError(t, c.GetClient().Create(ctx, &corev1.Namespace{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: nsAgents,
-			},
-		}))
-
-		// Create ClusterOutput pointing to test-receiver in control namespace
-		httpOut := v1beta1.ClusterOutput{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "http",
-				Namespace: nsControl,
-			},
-			Spec: v1beta1.ClusterOutputSpec{
-				OutputSpec: v1beta1.OutputSpec{
-					LoggingRef: "infra",
-					HTTPOutput: &output.HTTPOutputConfig{
-						Endpoint:    harness.ReceiverURL(release, tag),
-						ContentType: "application/json",
-						Buffer:      realTimeBuffer,
-					},
-				},
-			},
-		}
-		common.RequireNoError(t, c.GetClient().Create(ctx, &httpOut))
-
-		// Match producer pods cluster-wide
-		clusterFlow := v1beta1.ClusterFlow{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "flow",
-				Namespace: nsControl,
-			},
-			Spec: v1beta1.ClusterFlowSpec{
+	out := &v1beta1.ClusterOutput{
+		ObjectMeta: metav1.ObjectMeta{Name: "http", Namespace: nsControl},
+		Spec: v1beta1.ClusterOutputSpec{
+			OutputSpec: v1beta1.OutputSpec{
 				LoggingRef: "infra",
-				Match: []v1beta1.ClusterMatch{
-					{
-						ClusterSelect: &v1beta1.ClusterSelect{
-							Labels: producerLabels,
-						},
-					},
-				},
-				GlobalOutputRefs: []string{httpOut.Name},
-			},
-		}
-		common.RequireNoError(t, c.GetClient().Create(ctx, &clusterFlow))
-
-		// Create FluentBit agent (standalone) bound to the same loggingRef
-		agent := v1beta1.FluentbitAgent{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: "infra",
-			},
-			Spec: v1beta1.FluentbitSpec{
-				LoggingRef: "infra",
-				ConfigHotReload: &v1beta1.HotReload{
-					Image: image.ConfigReloader().Spec(),
-				},
-				BufferVolumeImage: image.NodeExporter().Spec(),
-			},
-		}
-		common.RequireNoError(t, c.GetClient().Create(ctx, &agent))
-
-		// Create Logging with control namespace and dedicated FluentbitAgentNamespace
-		logging := v1beta1.Logging{
-			ObjectMeta: metav1.ObjectMeta{Name: "infra"},
-			Spec: v1beta1.LoggingSpec{
-				LoggingRef:              "infra",
-				ControlNamespace:        nsControl,
-				FluentbitAgentNamespace: nsAgents,
-				FluentdSpec: &v1beta1.FluentdSpec{
-					Image:               image.Fluentd().Spec(),
-					ConfigReloaderImage: image.ConfigReloader().Spec(),
-					BufferVolumeImage:   image.NodeExporter().Spec(),
-					DisablePvc:          true,
-					Resources: corev1.ResourceRequirements{Requests: corev1.ResourceList{
-						corev1.ResourceCPU:    apiresource.MustParse("50m"),
-						corev1.ResourceMemory: apiresource.MustParse("50M"),
-					}},
+				HTTPOutput: &output.HTTPOutputConfig{
+					Endpoint:    env.Receiver.URL(tag),
+					ContentType: "application/json",
+					Buffer:      realTimeBuffer(),
 				},
 			},
-		}
-		common.RequireNoError(t, c.GetClient().Create(ctx, &logging))
+		},
+	}
+	env.Create(out)
 
-		// Start a log producer in the default namespace
-		setup.LogProducer(t, c.GetClient(), setup.LogProducerOptionFunc(func(options *setup.LogProducerOptions) {
-			options.Namespace = "default"
-			options.Labels = producerLabels
-		}))
-
-		fluentBitLabels := map[string]string{
-			types.NameLabel: "fluentbit",
-		}
-		aggregatorLabels := map[string]string{
-			types.NameLabel:      "fluentd",
-			types.ComponentLabel: "fluentd",
-		}
-		operatorLabels := map[string]string{
-			types.NameLabel: release,
-		}
-
-		require.Eventually(t, func() bool {
-			if operatorRunning := wait.AnyPodShouldBeRunning(t, c.GetClient(), client.MatchingLabels(operatorLabels))(); !operatorRunning {
-				t.Log("waiting for the operator")
-				return false
-			}
-			if producerRunning := wait.AnyPodShouldBeRunning(t, c.GetClient(), client.MatchingLabels(producerLabels))(); !producerRunning {
-				t.Log("waiting for the producer")
-				return false
-			}
-			if aggregatorRunning := wait.AnyPodShouldBeRunning(t, c.GetClient(), client.MatchingLabels(aggregatorLabels), client.InNamespace(nsControl)); !aggregatorRunning() {
-				t.Log("waiting for the aggregator in control namespace")
-				return false
-			}
-			if fluentbitRunning := wait.AnyPodShouldBeRunning(t, c.GetClient(), client.MatchingLabels(fluentBitLabels), client.InNamespace(nsAgents)); !fluentbitRunning() {
-				t.Log("waiting for the fluentbit daemonset in fluentbit agent namespace")
-				return false
-			}
-
-			cmd := common.CmdEnv(exec.Command("kubectl",
-				"logs",
-				"-n", nsControl,
-				"--tail", "30",
-				"-l", fmt.Sprintf("%s=%s", types.NameLabel, harness.ReceiverName(release))), c)
-			rawOut, err := cmd.Output()
-			if err != nil {
-				t.Logf("failed to get log consumer logs: %v", err)
-				return false
-			}
-			t.Logf("log consumer logs: %s", rawOut)
-			return strings.Contains(string(rawOut), tag)
-		}, 5*time.Minute, 3*time.Second)
-	}, func(t *testing.T, c common.Cluster) error {
-		path := filepath.Join(TestTempDir, fmt.Sprintf("cluster-%s.log", t.Name()))
-		t.Logf("Printing cluster logs to %s", path)
-		err := c.PrintLogs(common.PrintLogConfig{
-			Namespaces: []string{nsControl, nsAgents, "default"},
-			FilePath:   path,
-			Limit:      100 * 1000,
-		})
-		if err != nil {
-			return err
-		}
-
-		loggingOperatorName := "logging-operator-" + release
-		t.Logf("Collecting coverage files from logging-operator: %s/%s", nsControl, loggingOperatorName)
-		err = c.CollectTestCoverageFiles(nsControl, loggingOperatorName)
-		if err != nil {
-			t.Logf("Failed collecting coverage files: %s", err)
-		}
-		return nil
-	}, func(o *cluster.Options) {
-		if o.Scheme == nil {
-			o.Scheme = runtime.NewScheme()
-		}
-		common.RequireNoError(t, v1beta1.AddToScheme(o.Scheme))
-		common.RequireNoError(t, apiextensionsv1.AddToScheme(o.Scheme))
-		common.RequireNoError(t, appsv1.AddToScheme(o.Scheme))
-		common.RequireNoError(t, batchv1.AddToScheme(o.Scheme))
-		common.RequireNoError(t, corev1.AddToScheme(o.Scheme))
-		common.RequireNoError(t, rbacv1.AddToScheme(o.Scheme))
+	env.Create(&v1beta1.ClusterFlow{
+		ObjectMeta: metav1.ObjectMeta{Name: "flow", Namespace: nsControl},
+		Spec: v1beta1.ClusterFlowSpec{
+			LoggingRef: "infra",
+			Match: []v1beta1.ClusterMatch{
+				{ClusterSelect: &v1beta1.ClusterSelect{Labels: producerLabels}},
+			},
+			GlobalOutputRefs: []string{out.Name},
+		},
 	})
+
+	env.Create(&v1beta1.FluentbitAgent{
+		ObjectMeta: metav1.ObjectMeta{Name: "infra"},
+		Spec: v1beta1.FluentbitSpec{
+			LoggingRef: "infra",
+			ConfigHotReload: &v1beta1.HotReload{
+				Image: image.ConfigReloader().Spec(),
+			},
+			BufferVolumeImage: image.NodeExporter().Spec(),
+		},
+	})
+
+	env.Create(&v1beta1.Logging{
+		ObjectMeta: metav1.ObjectMeta{Name: "infra"},
+		Spec: v1beta1.LoggingSpec{
+			LoggingRef:              "infra",
+			ControlNamespace:        nsControl,
+			FluentbitAgentNamespace: nsAgents,
+			FluentdSpec: &v1beta1.FluentdSpec{
+				Image:               image.Fluentd().Spec(),
+				ConfigReloaderImage: image.ConfigReloader().Spec(),
+				BufferVolumeImage:   image.NodeExporter().Spec(),
+				DisablePvc:          true,
+				Resources: corev1.ResourceRequirements{Requests: corev1.ResourceList{
+					corev1.ResourceCPU:    resource.MustParse("50m"),
+					corev1.ResourceMemory: resource.MustParse("50M"),
+				}},
+			},
+		},
+	})
+
+	env.StartLogProducer(nsProducer, producerLabels)
+
+	env.WaitFor(
+		wait.Operator(release),
+		wait.Producer(producerLabels),
+		wait.FluentdAggregator(nsControl),
+		wait.Fluentbit(nsAgents),
+	)
+
+	env.Receiver.MustReceive(tag)
 }
